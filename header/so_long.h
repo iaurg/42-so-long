91c975993a82cc325f071e76d23f09809587f110
Italo aurelio <italoaurelior@gmail.com>
2021-09-19 12:06:58 -0300

    Add new lib and functions

--- header/so_long.h
+++ header/so_long.h
@@ -1,7 +1,8 @@
 #ifndef SO_LONG_H
 # define SO_LONG_H
-# include <stdarg.h>
-# include <unistd.h>
-# include "../mlx/mlx.h"
-
+# include <stdio.h>
+# include "../libs/mlx/mlx.h"
+# include "../libs/libft/libft.h"
+int	print_error(char *message);
+int	validate_map(char *map_file);
 #endif
