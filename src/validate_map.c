9e9c14b8e1bb2b989335ee94a1842a036c1fce95
Italo aurelio <italoaurelior@gmail.com>
2021-09-22 21:47:58 -0300

    Validating walls

--- src/validate_map.c
+++ src/validate_map.c
@@ -27,34 +27,48 @@ static int is_valid_extension(char *map_file)
 	return(0);
 }
 
-static int is_valid_wall(char *map_file)
+static char **generate_map_array(char *map_file)
 {
 	int file_descriptor;
 	char *temp;
+	char *map_string;
+	char *temp_map;
 
 	file_descriptor = open(map_file, O_RDONLY);
-	if (file_descriptor < 0)
-	{
-		print_error("Fail to open map file");
-		return (0);
-	}
-	/*
-	How can I use split here? Separate each line in a array.
-	*/
+	map_string = ft_strdup("");
 	while (1)
 	{
 		temp = get_next_line(file_descriptor);
-		printf("%s", temp);
+		temp_map = map_string;
+		if (temp)
+			map_string = ft_strjoin(temp_map, temp);
 		free(temp);
 		if (!temp) break;
 	}
-	print_error("Invalid map walls");
-	return(0);
+	return(ft_split(map_string, '\n'));
 }
+
+static int is_valid_wall(char *map_file)
+{
+	char **tst;
+	int i;
+
+	tst = generate_map_array(map_file);
+	i = 0;
+	while(tst[i])
+	{
+		printf("test: %s\n", tst[i]);
+		i++;
+	}
+
+	return(print_error("Invalid map walls"));
+}s
 
 int	validate_map(char *map_file)
 {
-	if(!is_valid_extension(map_file) || !is_valid_wall(map_file))
+	if(!is_valid_extension(map_file))
+		return (0); // 1 if is a valid map file
+	if(!is_valid_wall(map_file))
 		return (0); // 1 if is a valid map file
 	return (1);
 }
