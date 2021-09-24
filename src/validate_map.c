f63cbb0803b9652055e6b77225a5e3eefc7c6ac3
Italo aurelio <italoaurelior@gmail.com>
2021-09-23 23:09:18 -0300

    Add more validations and use map as pointer

--- src/validate_map.c
+++ src/validate_map.c
@@ -27,34 +27,51 @@ int is_valid_extension(char *map_file)
 	return(0);
 }
 
-static int is_valid_wall(char **game_map)
+static int is_valid_wall(t_game *game)
 {
 	int i;
-	int total_lines;
-
+	int total_row;
+	int total_col;
 	i = 0;
-	total_lines = -1;
-	while(game_map[i])
-	{
-		// printf("test: %s\n", game_map[i]);
-		total_lines++;
-		i++;
-	}
+	total_row = game->map.row;
+	total_col = game->map.col;
 
-	i = 0;
-	while (game_map[0][i])
-		if (game_map[0][i++] != '1')
+	while (game->map.map_array[0][i])
+		if (game->map.map_array[0][i++] != '1')
 			return (print_error("Invalid line"));
 	i = 0;
-	while (game_map[total_lines][i])
-		if (game_map[total_lines][i++] != '1')
+	while (game->map.map_array[total_row - 1][i])
+		if (game->map.map_array[total_row - 1][i++] != '1')
 			return (print_error("Invalid line"));
+	printf("row: %d\n", total_row);
+	printf("col: %d\n", total_col);
 	return (print_error("Invalid map walls"));
 }
 
-int	validate_map(t_game game)
+static int is_valid_rectangle(t_game *game)
 {
-	if(!is_valid_wall(game.map))
+	if ((game->map.row <= 0) || (game->map.col <= 0))
+		return (print_error("Invalid map row or col"));
+	return(1);
+}
+
+static void count_map_sides(t_game *game)
+{
+	int total_row;
+	int total_col;
+	total_row = -1;
+	total_col = 0;
+
+	while (game->map.map_array[++total_row]);
+	while (game->map.map_array[0][++total_col]);
+	game->map.row = total_row;
+	game->map.col = total_col;
+}
+
+int	validate_map(t_game *game)
+{
+	count_map_sides(game);
+	if(!is_valid_wall(game) || !is_valid_rectangle(game))
 		return (0); // 1 if is a valid map file
 	return (1);
 }
