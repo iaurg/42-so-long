e6fcbc2ea0f59d997861a8581ec5f095f6a917e0
Italo aurelio <italoaurelior@gmail.com>
2021-09-25 20:49:51 -0300

    Validate norminette and refactor

--- src/validate_map.c
+++ src/validate_map.c
@@ -1,37 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   validate_map.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/25 20:34:48 by itaureli          #+#    #+#             */
+/*   Updated: 2021/09/25 20:39:25 by itaureli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../header/so_long.h"
 
-/*
-Validate map extension .ber - OK
-Validate map walls - OK
-Validate retangular map - OK
-Validate map elements - OK
-Validate Map must have at E,C,P - OK
-*/
-
-int is_valid_extension(char *map_file)
+int	is_valid_extension(char *map_file)
 {
-	char *ext;
+	char	*ext;
 
-	if(!map_file)
+	if (!map_file)
 		return (0);
 	ext = ft_strrchr(map_file, '.');
 	if (ft_strncmp(ext, ".ber", 5) == 0)
 		return (1);
 	print_error("Invalid map extension");
-	return(0);
+	return (0);
 }
 
 static int	is_valid_wall(t_game *game)
 {
 	int	i;
-	int	row;
-	int	col;
 	int	lst_line;
 
 	i = 0;
-	row = game->map.row;
-	col = game->map.col;
-	lst_line = row - 1;
+	lst_line = game->map.row - 1;
 	while (game->map.map_array[0][i] && game->map.map_array[lst_line][i])
 	{
 		if (game->map.map_array[0][i] != '1')
@@ -43,78 +43,75 @@ static int	is_valid_wall(t_game *game)
 	i = 0;
 	while (i <= lst_line)
 	{
-		if (game->map.map_array[i][col - 1] != '1' ||
+		if (game->map.map_array[i][game->map.col - 1] != '1' ||
 			game->map.map_array[i][0] != '1')
 			return (print_error("Invalid line"));
 		i++;
 	}
 	return (1);
 }
-
-static int is_valid_rectangle(t_game *game)
+/*
+static int	is_valid_rectangle(t_game *game)
 {
 	if ((game->map.row < 5) || (game->map.col < 3))
 		return (print_error("Invalid map row or col"));
-	return(1);
+	return (1);
 }
+*/
 
 static int	is_valid_elements(t_game *game)
 {
-	int	i;
-	int	j;
-	int count_elements;
-	char element;
+	int		i;
+	int		j;
+	int		count_elements;
 
 	i = 0;
 	j = 0;
 	count_elements = 0;
-
-	while(i < game->map.row)
+	while (i < game->map.row)
 	{
-		while(j < game->map.col)
+		while (j < game->map.col)
 		{
-			element = game->map.map_array[i][j];
-			if (element != PLAYER_CHAR
-				&& element != COIN_CHAR
-				&& element != WALL_CHAR
-				&& element != FREE_CHAR
-				&& element != EXIT_CHAR)
-				return(print_error("Invalid element in map"));
-			j++;
-			if(element == PLAYER_CHAR
-				|| element == COIN_CHAR
-				|| element == EXIT_CHAR)
-			{
+			if (game->map.map_array[i][j] != PLAYER_CHAR
+				&& game->map.map_array[i][j] != COIN_CHAR
+				&& game->map.map_array[i][j] != WALL_CHAR
+				&& game->map.map_array[i][j] != FREE_CHAR
+				&& game->map.map_array[i][j] != EXIT_CHAR)
+				return (print_error("Invalid element in map"));
+			if (game->map.map_array[i][j] == PLAYER_CHAR
+				|| game->map.map_array[i][j] == COIN_CHAR
+				|| game->map.map_array[i][j] == EXIT_CHAR)
 				count_elements++;
-			}
+			j++;
 		}
 		j = 0;
 		i++;
 	}
 	if (count_elements < 3)
-		return(print_error("Map must have at least one E, C, and one P"));
-	return(1);
+		return (print_error("Map must have at least one E, C, and one P"));
+	return (1);
 }
 
-static void count_map_sides(t_game *game)
+static void	count_map_sides(t_game *game)
 {
-	int total_row;
-	int total_col;
+	int	total_row;
+	int	total_col;
+
 	total_row = 0;
 	total_col = 0;
-
-	while (game->map.map_array[++total_row]);
-	while (game->map.map_array[0][++total_col]);
+	while (game->map.map_array[++total_row])
+		;
+	while (game->map.map_array[0][++total_col])
+		;
 	game->map.row = total_row;
 	game->map.col = total_col;
 }
 
 int	validate_map(t_game *game)
 {
-	count_map_sides(game);
-	if(!is_valid_wall(game)
-		|| !is_valid_elements(game)
-		|| !is_valid_rectangle(game))
-		return (0); // 1 if is a valid map file
+	count_map_sides (game);
+	if (!is_valid_wall(game)
+		|| !is_valid_elements(game))
+		return (0);
 	return (1);
 }
