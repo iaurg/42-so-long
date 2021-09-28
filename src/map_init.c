3df0e6f45a686bc7d7f84c4c32139e3e5635f9c6
Italo aurelio <italoaurelior@gmail.com>
2021-09-28 06:44:43 -0300

    Adjust map init logic

--- src/map_init.c
+++ src/map_init.c
@@ -2,21 +2,24 @@
 
 char **generate_map_array(char *map_file)
 {
-	int file_descriptor;
-	char *temp;
-	char *map_string;
-	char *temp_map;
+	int		file_descriptor;
+	char	*temp;
+	char	*map_string;
+	char	*temp_map;
+	char	**result;
 
 	file_descriptor = open(map_file, O_RDONLY);
 	map_string = ft_strdup("");
 	while (1)
 	{
 		temp = get_next_line(file_descriptor);
-		temp_map = map_string;
-		if (temp)
-			map_string = ft_strjoin(temp_map, temp);
-		free(temp);
 		if (!temp) break;
+		temp_map = map_string;
+		map_string = ft_strjoin(temp_map, temp);
+		free(temp);
+		free(temp_map);
 	}
-	return(ft_split(map_string, '\n'));
+	result = ft_split(map_string, '\n');
+	close(file_descriptor);
+	return (result);
 }
