2926d6ff23a10af92f17aa014f8ea0bbfb15bde3
Italo aurelio <italoaurelior@gmail.com>
2021-09-28 06:47:57 -0300

    First image print

--- src/img_init.c
+++ src/img_init.c
@@ -6,7 +6,7 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/27 21:50:55 by itaureli          #+#    #+#             */
-/*   Updated: 2021/09/27 22:46:19 by itaureli         ###   ########.fr       */
+/*   Updated: 2021/09/28 06:47:20 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,10 @@
 
 void	img_init(t_game *game)
 {
-	void *img;
-	int width;
-	int height;
-	img = mlx_xpm_file_to_image(game->mlx, "../assets/sprites/P.xpm", &width, &height);
-	if(!img)
-		print_error("Error to read image");
-	mlx_put_image_to_window(game->mlx, game->screen, img, 1 * 32, 1 * 32);
+	void	*img;
+	int		img_width;
+	int 	img_height;
+
+	img = mlx_xpm_file_to_image(game->mlx, WALL_IMG, &img_width, &img_height);
+	mlx_put_image_to_window(game->mlx, game->screen, img, 0, 0);
 }
