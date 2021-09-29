8e642492a4d8874ea41b706250c6f09bbf0384ae
Italo aurelio <italoaurelior@gmail.com>
2021-09-29 06:44:39 -0300

    Remove comments

--- src/game_init.c
+++ src/game_init.c
@@ -6,7 +6,7 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 21:00:50 by itaureli          #+#    #+#             */
-/*   Updated: 2021/09/26 21:18:28 by itaureli         ###   ########.fr       */
+/*   Updated: 2021/09/29 06:44:31 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,11 @@
 
 void	window_init(t_game *game)
 {
-	int width; //x
-	int height; //y
+	int	width;
+	int	height;
 
 	width = game->map.col * SPRITE_SIZE;
 	height = game->map.row * SPRITE_SIZE;
-	// If mlx_init fails to set up the connection to the graphical system, it will return NULL, otherwise a non-null pointer is returned as a connection identifier.
 	game->mlx = mlx_init();
-	// To calculate screen size get number of rows and columns x tiles size
 	game->screen = mlx_new_window(game->mlx, width, height, "so_long");
 }
