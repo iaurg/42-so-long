5da06c040e669318cd7a367d6415140cea82cf2a
Italo aurelio <italoaurelior@gmail.com>
2021-09-29 06:43:04 -0300

    Add hook to close on click X

--- src/key_hooks.c
+++ src/key_hooks.c
@@ -6,7 +6,7 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 22:45:18 by itaureli          #+#    #+#             */
-/*   Updated: 2021/09/26 22:56:54 by itaureli         ###   ########.fr       */
+/*   Updated: 2021/09/29 06:41:26 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ int key_hook(int keycode, t_game *game)
 
 int key_hooks (t_game *game)
 {
-	mlx_hook(game->screen, 2, 1L << 0, &key_hook, &game);
+	mlx_hook(game->screen, EVENT_KEY_PRESS, 1L << 0, &key_hook, &game);
+	mlx_hook(game->screen, EVENT_X_WIN_PRESS, 1L << 2, &close_game, &game);
 	return(0);
 }
