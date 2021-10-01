b3ba07bf557a0b2d6b95e5d17ab03d5e0e6ed4b5
Italo aurelio <italoaurelior@gmail.com>
2021-10-01 06:26:55 -0300

    Can move into map

--- src/move_elements.c
+++ src/move_elements.c
@@ -6,7 +6,7 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 09:01:58 by itaureli          #+#    #+#             */
-/*   Updated: 2021/09/30 23:07:51 by itaureli         ###   ########.fr       */
+/*   Updated: 2021/10/01 06:26:49 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,11 @@ static void move_left(t_game *game)
 
 	py = game->player.y;
 	px = game->player.x;
-
 	if (game->map.map_array[py][px - 1] == WALL_CHAR)
 		return;
 	game->map.map_array[py][px - 1] = PLAYER_CHAR;
-	game->map.map_array[py][px] = FREE_CHAR;
 	game->player.x = px - 1;
-	if (game->map.map_array[py][px - 2] != WALL_CHAR)
-		game->map.map_array[py][px - 1] = FREE_CHAR;
+	game->map.map_array[py][px] = FREE_CHAR;
 	render_images(game);
 }
 
@@ -48,6 +45,40 @@ static void move_right(t_game *game)
 	render_images(game);
 }
 
+static void move_up(t_game *game)
+{
+	int py;
+	int px;
+
+	py = game->player.y;
+	px = game->player.x;
+
+	if (game->map.map_array[py - 1][px] == WALL_CHAR)
+		return;
+	game->map.map_array[py - 1][px] = PLAYER_CHAR;
+	game->player.y = py - 1;
+	if (game->map.map_array[py - 1][px] != WALL_CHAR)
+		game->map.map_array[py][px] = FREE_CHAR;
+	render_images(game);
+}
+
+static void move_down(t_game *game)
+{
+	int py;
+	int px;
+
+	py = game->player.y;
+	px = game->player.x;
+
+	if (game->map.map_array[py + 1][px] == WALL_CHAR)
+		return;
+	game->map.map_array[py + 1][px] = PLAYER_CHAR;
+	game->player.y = py + 1;
+	if (game->map.map_array[py + 1][px] != WALL_CHAR)
+		game->map.map_array[py][px] = FREE_CHAR;
+	render_images(game);
+}
+
 void	move_elements(int keycode, t_game *game)
 {
 	if (keycode == KEY_A)
@@ -55,9 +86,9 @@ void	move_elements(int keycode, t_game *game)
 	else if (keycode == KEY_D)
 		move_right(game);
 	else if (keycode == KEY_W)
-		printf("move_up");
+		move_up(game);
 	else if (keycode == KEY_S)
-		printf("move_down");
+		move_down(game);
 	if (!game)
 		return;
 }
