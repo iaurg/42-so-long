7e571135a7619cae32a2db6916de25e6637bcf18
Italo aurelio <italoaurelior@gmail.com>
2021-11-27 16:22:54 -0300

    change function to static

--- src/key_hooks.c
+++ src/key_hooks.c
@@ -6,13 +6,13 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 22:45:18 by itaureli          #+#    #+#             */
-/*   Updated: 2021/10/02 08:57:40 by itaureli         ###   ########.fr       */
+/*   Updated: 2021/11/27 16:22:43 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/so_long.h"
 
-int	key_hook(int keycode, t_game *game)
+static int	key_hook(int keycode, t_game *game)
 {
 	if (keycode == KEY_ESC)
 		close_game(game);
